390f15d290ce4df7a0699af43b0ded56eff773b2
ERNESTO <viatsat@gmail.com>
2020-11-12 10:28:16 -0300

    inicio

---
+++ my-app/src/Components/Productos.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Productos extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
