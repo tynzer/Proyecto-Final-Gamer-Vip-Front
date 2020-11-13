618d2a8d0b9e38dd556e27b8ade67c12841d3936
Ernesto <ernesto@gmail.com>
2020-11-13 20:37:40 -0300

    nosotros

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,11 +4,11 @@ import { SocialIcon } from 'react-social-icons';
 class Footer extends Component {
     render() {
         return (
-            <footer className="py-5 bg-dark">
+            <footer className="py-3 bg-dark">
                 <div className="container">
                     <p className="m-0 text-center text-white">Copyright © 2020</p>
                     <div className="m-0 text-center text-white">
-                        <SocialIcon url="https://www.facebook.com/gamer.vip.arg/" />   <SocialIcon url="https://www.instagram.com/gamer.vip.arg/" />   <SocialIcon url="https://www.twitch.tv/gamerviparg" />
+                        <SocialIcon url="https://www.facebook.com/gamer.vip.arg/" target="_blank" />   <SocialIcon url="https://www.instagram.com/gamer.vip.arg/" target="_blank"/>   <SocialIcon url="https://www.twitch.tv/gamerviparg" target="_blank"/>
                     </div>
                 </div>
             </footer>
--- src/Components/Nosotros.js
+++ src/Components/Nosotros.js
@@ -26,9 +26,16 @@ export default class Nosotros extends Component {
             (https://www.atlassian.com/es/agile/project-management/epics)
             Una EPIC es una etiqueta que agrupa tareas individuales con un mismo objetivo.
             </p>
+            <h2>Mapa</h2>
+                <p className="h5">Avenida Corrientes 6237, CABA.</p> 
+                <div className="mapa"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3284.5125198743117!2d-58.45006848429093!3d-34.59119936434371!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb5f12e398ecd%3A0x98abf435fbfab70!2sAv.%20Corrientes%206237%2C%20C1427BPA%20CABA!5e0!3m2!1ses-419!2sar!4v1594497150374!5m2!1ses-419!2sar" title="myFrame" width={1100} height={400} frameBorder={0} style={{border: 0}} allowFullScreen="" aria-hidden="false" tabIndex={0} /></div>
+                <h2>Redes Sociales</h2>
+                <p><a href="https://www.facebook.com/gamer.vip.arg/" target="_blank">FaceBook-GamerVip</a></p>
+                <p><a href="https://www.twitch.tv/gamerviparg" target="_blank">Twitch-GamerVip</a></p>
+                <p><a href="https://www.instagram.com/gamer.vip.arg/" target="_blank">Instagram-GamerVip</a></p>
           </div>
         </div>
-
+        
       </div>
     );
   }
