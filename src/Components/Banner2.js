97da39c9ee17fb94c5406c6255a7c884f1087932
Tynzer <martinsola119@gmail.com>
2020-11-18 23:13:21 -0300

    redirige

--- src/Components/Banner2.js
+++ src/Components/Banner2.js
@@ -5,38 +5,47 @@ import { Redirect } from "react-router-dom";
 
 class Banner2 extends Component {
 
-  handleClick = (producto ) =>{
- /*    e.preventDefault(); */
-    let categoriaTrim = producto.trim();
-    let categoriaReplace = categoriaTrim.replace(/\s/g, "-");
-return (<Redirect to={"/productos/"+ categoriaReplace} />)
+  constructor(props) {
+    super(props);
+    this.state = {
+      redireccionar: false,
+      categoriaReplace: "",
+    }
   }
 
+  handleClick = (producto) => {   
+    let categoriaTrim = producto.trim();
+    let categoriaReplace = categoriaTrim.replace(/\s/g, "-");
+    this.setState({ redireccionar: true, categoriaReplace: categoriaReplace })
+  }
 
-    render() {
-        return (
-            <div>
-                <section>
-  <div className="wrapper">
-    <h1>Productos Destacados</h1>
-  </div>
-
-  <tile-slider>
-   {this.props.productos.map(producto => { 
-                                        return(                  
-                                          <game-tile href="#" onClick={(e) => { e.preventDefault()
-                                             this.handleClick(producto.categoria) }} platform={producto.marca} loading="auto" image={producto.linkImagen}>                                            
-                                          <h2 slot="title">{producto.titulo}</h2>
-                                          </game-tile>
-                                          )})} 
-                                            
-  </tile-slider>
- 
-</section>
-
+  render() {
+    if (this.state.redireccionar) {
+      return (<Redirect to={"/productos/" + this.state.categoriaReplace} />)
+    } else {
+      return (
+        <div>
+          <section>
+            <div className="wrapper">
+              <h1>Productos Destacados</h1>
             </div>
-        );
+            <tile-slider>
+              {this.props.productos.map(producto => {
+                return (
+                  <game-tile href="#" onClick={(e) => {
+                    e.preventDefault()
+                    this.handleClick(producto.categoria)
+                  }} platform={producto.marca} loading="auto" image={producto.linkImagen}>
+                    <h2 slot="title">{producto.titulo}</h2>
+                  </game-tile>
+                )
+              })}
+            </tile-slider>
+          </section>
+        </div>
+      );
     }
+  }
 }
 
 export default Banner2;
