47ea58d666216790b0c2173174c8d469a8b6b865
Tynzer <martinsola119@gmail.com>
2020-11-18 23:22:40 -0300

    wasap fix

--- src/Components/Whatsapp2.js
+++ src/Components/Whatsapp2.js
@@ -3,8 +3,9 @@ import WhatsAppWidget from 'react-whatsapp-widget'
 import 'react-whatsapp-widget/dist/index.css'
  
 const Whatsapp2 = () => {
-  return <WhatsAppWidget phoneNumber='+549388435140' />
+  return <WhatsAppWidget phoneNumber='+54 9 114444 4444' message= 'Hola que tal? En que podemos ayudarte'
+  textReplyTime= 'Normalmente respondemos en el día' companyName= 'Gamer VIP'
+  />
 }
- 
 
 export default Whatsapp2;
