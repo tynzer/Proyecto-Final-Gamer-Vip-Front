ecd2d9241ac6bf95929ad229a1c28284a2651511
lgLuis <luis_unlz@yahoo.com.ar>
2020-11-19 12:42:18 -0300

    home 1er parte

--- src/Components/Banner2.js
+++ src/Components/Banner2.js
@@ -1,6 +1,63 @@
 import React, { Component } from 'react';
 import '../banner2.css';
 import { Redirect } from "react-router-dom";
+import './styles/banner2.css'
+
+
+class Banner2 extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      redireccionar: false,
+      categoriaReplace: "",
+    }
+  }
+
+  handleClick = (producto) => {   
+    let categoriaTrim = producto.trim();
+    let categoriaReplace = categoriaTrim.replace(/\s/g, "-");
+    this.setState({ redireccionar: true, categoriaReplace: categoriaReplace })
+  }
+
+  render() {
+    if (this.state.redireccionar) {
+      return (<Redirect to={"/productos/" + this.state.categoriaReplace} />)
+    } else {
+      return (
+        <div className="banner2-font">
+          <section>
+            <div className="wrapper">
+              <h1>Productos Destacados</h1>
+            </div>
+            <tile-slider>
+              {this.props.productos.map(producto => {
+                return (
+                  <game-tile href="#" onClick={(e) => {
+                    e.preventDefault()
+                    this.handleClick(producto.categoria)
+                  }} platform={producto.marca} loading="auto" image={producto.linkImagen}>
+                    <h2 className="banner2-font" slot="title">{producto.titulo}</h2>
+                  </game-tile>
+                )
+              })}
+            </tile-slider>
+          </section>
+        </div>
+      );
+    }
+  }
+}
+
+export default Banner2;
+
+
+
+
+/*
+import React, { Component } from 'react';
+import '../banner2.css';
+import { Redirect } from "react-router-dom";
 
 
 class Banner2 extends Component {
@@ -49,3 +106,5 @@ class Banner2 extends Component {
 }
 
 export default Banner2;
+
+*/
