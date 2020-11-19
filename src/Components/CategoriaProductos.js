6b1df38df5b8e2a9975276c94f3eeee1f57a9f19
Tynzer <martinsola119@gmail.com>
2020-11-18 23:30:29 -0300

    mimsmo css que productos

--- src/Components/CategoriaProductos.js
+++ src/Components/CategoriaProductos.js
@@ -1,31 +1,33 @@
 import React, { Component } from 'react'
 import Categorias from './Categorias'
 /* import Producto from './Producto'; */
+import './styles/productos.css'
 
-export default class CategoriaProductos extends Component {
+class CategoriaProductos extends Component {
     
     render() {
         return (
             <div>
-                    <div className=" container-fluid">
+                <div className="pt-5 productos-fondo">
+                    <div className="container">
                         <div className="row">
                             <Categorias categorias={this.props.categorias}/>
                             <div className="col-lg-9 mt-5">
-                                <div className="row mt-5">
+                                <div className="row mt-5 ">
                                     {this.props.productos.map(producto => { 
                                         return(
                                             <div key={producto._id} className="col-lg-4 col-md-6 mb-4">
-                                                <div className="card h-100">
+                                                <div className="card h-100 productos-borde">
                                                     <img className="card-img-top" src={producto.linkImagen} alt={producto.titulo} />
-                                                    <div className="card-body">
-                                                        <h4 className="card-title  text-primary">
+                                                    <div className="card-body productos-item-fondo">
+                                                        <h4 className="card-title text-primary ">
                                                             {producto.titulo}
                                                         </h4>
                                                         <h5>${producto.precio}</h5>
                                                         <p className="card-text">{producto.descripcion}</p>
                                                     </div>
                                                     <div className="card-footer">
-                                                        <small className="text-muted">★ ★ ★ ★ ★</small>
+                                                        <small className="productos-estrellas">★ ★ ★ ★ ★</small>
                                                     </div>
                                                 </div>
                                             </div>    
@@ -34,7 +36,9 @@ export default class CategoriaProductos extends Component {
                             </div>
                         </div>
                     </div>
+                </div>
             </div>
         );
     }
 }
+export default CategoriaProductos
